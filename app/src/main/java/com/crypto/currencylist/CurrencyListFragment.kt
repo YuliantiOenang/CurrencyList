5d3b31a03f3de889c75c7edc51666a5bc9acf616
YuliantiOenang <yoenang@seekerscapital.com>
2024-06-29 00:59:22 +0800

    demo activity and currency list fragment init

--- app/src/main/java/com/crypto/currencylist/CurrencyListFragment.kt
+++ app/src/main/java/com/crypto/currencylist/CurrencyListFragment.kt
@@ -1,4 +1,28 @@
 package com.crypto.currencylist
 
-class CurrencyListFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+import com.crypto.currencylist.databinding.FragmentCurrencyListBinding
+
+class CurrencyListFragment: Fragment() {
+
+    private var _binding: FragmentCurrencyListBinding? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = DataBindingUtil.inflate(inflater, R.layout.fragment_currency_list, container, false)
+        (_binding as? ViewDataBinding)?.apply {
+            lifecycleOwner = viewLifecycleOwner
+//            setVariable(BR.vm, viewModel)
+        }
+        return requireNotNull(_binding).root
+    }
 }
--- app/src/main/java/com/crypto/currencylist/DemoActivity.kt
+++ app/src/main/java/com/crypto/currencylist/DemoActivity.kt
@@ -1,7 +1,16 @@
 package com.crypto.currencylist
 
 import android.app.Activity
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.crypto.currencylist.databinding.ActivityDemoBinding
 
 class DemoActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDemoBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityDemoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
 }
